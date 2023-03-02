f744346f89d2fc3f9e99decc9c5213f88b875014
fernando.henning <fernando.henning@vantis.mx>
2023-03-01 22:11:46 -0600

    Added details form and video player

--- frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, FormsModule} from "@angular/forms";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {MatChipInputEvent} from "@angular/material/chips";
 
 @Component({
   selector: 'app-save-video-details',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./save-video-details.component.css']
 })
 export class SaveVideoDetailsComponent implements OnInit {
+  saveVideoDetailsForm: FormGroup;
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('')
+  videoStatus: FormControl = new FormControl('')
 
-  constructor() { }
+  addOnBlur = true;
 
-  ngOnInit(): void {
+  constructor() {
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      videoStatus: this.videoStatus,
+    })
+  }
+
+  ngOnInit(): void {}
+
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  tags: string[] = [];
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.tags.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(tags: string): void {
+    const index = this.tags.indexOf(tags);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
   }
 
 }
